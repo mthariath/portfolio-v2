28905f050f6bf57a603b6b9c2b5cf8cb9da5da3b
Michael Thariath <mictha1@gmail.com>
2019-02-07 20:26:26 -0600

    added redux and socketio icons, added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Layout, SEO } from '../components/'
+import { TitleBox, Paragraph, SadFace } from 'components'
 
 const NotFoundPage = ({ location }) => (
   <Layout location={location.pathname}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <TitleBox title="404">
+      <div
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <SadFace />
+        <Paragraph Center>
+          How embarrassing&hellip; A web developer with a broken link&hellip;
+        </Paragraph>
+      </div>
+    </TitleBox>
   </Layout>
 )
 NotFoundPage.propTypes = {
